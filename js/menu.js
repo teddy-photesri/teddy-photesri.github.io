401cffc288604cd9bd20d035bc02cecece99aff6
Teddy <teddy.photesri@gmail.com>
2020-02-18 09:49:00 -0800

    update

--- js/menu.js
+++ js/menu.js
@@ -32,3 +32,30 @@ function toggleMenu() {
   }
 }
 
+//
+//
+//
+// $('.panel-collapse').on('show.bs.collapse', function () {
+//    $(this).siblings('.panel-heading').addClass('active');
+//  });
+//
+//  $('.panel-collapse').on('hide.bs.collapse', function () {
+//    $(this).siblings('.panel-heading').removeClass('active');
+//  });
+//
+
+
+ // var acc = document.getElementsByClassName("accordion")
+ // var i;
+ // for(i = 0; i < acc.length; i++) {
+ //   acc[i].addEventListener("click", function(){
+ //     this.classList.toggle("active");
+ //
+ //     var panel = this.next.nextElementSibling;
+ //     if (panel.style.display === "block"){
+ //       panel.style.display = "none";
+ //     } else {
+ //       panel.style.display = "block";
+ //     }
+ //   });
+ // }
